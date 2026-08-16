6c85845616c09a68e43f8b0e6c14d6256868ba0f
Orell <orellbrooks0@gmail.com>
2026-08-15 23:35:33 -0400

    Finding Numbers

---
+++ python_fundamentals/02_finding_numbers.py
@@ -0,0 +1,8 @@
+# Problem 2: Find Numbers
+# Given a string containing words and numbers,
+# return a list containing only the numbers.
+#
+# Example:
+# Input: "Switch 1 has 24 ports and Switch 2 has 48 ports"
+# Output: [1, 24, 2, 48]
+

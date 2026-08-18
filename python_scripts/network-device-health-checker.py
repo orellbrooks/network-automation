9f01f25382fa6805236e78a80687f3f6a7e260a7
orellbrooks <orellbrooks0@gmail.com>
2026-08-18 15:17:21 -0400

    Update network-device-health-checker.py

--- python_scripts/network-device-health-checker.py
+++ python_scripts/network-device-health-checker.py
@@ -1,11 +1,24 @@
 from netmiko import ConnectHandler
 
-device = {"device_type": "cisco_ios",
-          "host": "Your_Ip",
-          "username": "your_user",
-          "password": "your_password"
-
-
+cisco_device = {"device_type": "cisco_ios",
+                "username": "ciscosw",
+                "password": "password123",
+                "host": "10.0.0.0"
 }
 
-connection = ConnectHandler(**device)
+net_connect = ConnectHandler(**cisco_device)
+
+output = net_connect.send_command("show version")
+
+print(output)
+
+version_line = "Cisco IOS Software, C3560 Software (C3560-IPSERVICESK9-M), Version 12.2(55)SE10, RELEASE SOFTWARE (fc1)"
+
+clean_data = version_line.split(",")
+
+for parts in clean_data:
+    if "Version" in parts:
+        version = parts.split("Version ")
+        answer = version[1]
+print(answer)
+
